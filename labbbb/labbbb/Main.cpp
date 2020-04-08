88d9bc10a868f36190ed3347f8897c6a3b5c299b
AdrianAlva <adrian.alvarez.minaya@ucsp.edu.pe>
2020-04-07 22:28:58 -0500

    tarea de clase

---
+++ labbbb/labbbb/Main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+/*int main() {
+	char a[150];
+	char c[150];
+	char d[150];
+	char b[150];
+	cout << "ingrese su nomre: " ;
+	cin.getline (a,150);
+	cout << "ingrese su dirección: ";
+	cin.getline(c, 150);
+	cout << "infrese su hobbie: ";
+	cin.getline(d, 150);
+	cout << "ingrse su numero: ";
+	cin >> b;
+	cout << "---------------------" << endl;
+	cout << a << endl;
+	cout << "numero: " << b << endl;
+	cout << "direccion: " << c << endl;
+	cout << "hobbie: " << d << endl;
+	return 0;
+
+}*/
+
+//calculadora
+/*int main() {
+	float a, c;
+	float suma, resta, multi, divi, raiza,raizb;
+	cout << "ingrese primer numero: ";
+	cin >> a;
+	cout << "ingrese segundo numero: ";
+	cin >> c;
+	suma = a + c;
+	resta = a - c;
+	multi = a * c;
+	divi = a / c;
+	raiza=sqrt(a);
+	raizb=sqrt(c);
+	cout << "la suma es: " << suma << endl;
+	cout << "la suma resta es: " << resta << endl;
+	cout << "la multiplicacion es: " << multi << endl;
+	cout << "la division es: " << divi << endl;
+	cout << "la raiz del primer digito es: " << raiza << endl;
+	cout << "la raiz del segundo digito es: " << raizb << endl;
+	return 0;
+}*/
+
+/*áreas
+int main() {
+	float a, b, c, d, areac, areat, areaci;
+	cout << "AREA DE UN CUADRADO " << endl;
+	cout << "ingresa el lado del cuadrado: " ;
+	cin >> a;
+	areac = a * a;
+	cout << "el area del cuadrado es: " << areac << endl ;
+	cout << "AREA DE UN TRIANGULO" << endl;
+	cout << "ingresa altura del triangulo: ";
+	cin >> b;
+	cout << "ingresa base del triangulo: ";
+	cin>> c;
+	areat = (a * b) / 2;
+	cout << "el area del tringulo es: " << areat << endl;
+	cout << "AREA DEL CIRCULO" << endl;
+	cout << "ingresa el radio del circulo: ";
+	cin >> d;
+	areaci = 3.14 * (d * d);
+	cout << "el area del circulo es : " << areaci << endl;
+	return 0;
+}
+*/
+
+
